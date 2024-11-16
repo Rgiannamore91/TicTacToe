5152a38706373f654cf71fa510078771fd055571
DESKTOP-2FH8J2L\houzi <houzidalao91@gmail.com>
2024-11-16 14:19:10 -0500

    init project

---
+++ funcs.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+std::vector<std::string> intro () {
+    std::cout << "This is Tic-Tac-Toe.\n" << "This game requires two players. Please enter their names: \n";
+    std::vector<std::string> returnValue;
+    while (returnValue.size() < 2) {
+        std::cout << ""
+    } 
+
+}
